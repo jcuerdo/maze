a30b044ca77664a31358a3317d7b6cecc0772fc3
Jorge Cuerdo Álvarez <jocual@gmail.com>
2016-02-17 20:07:34 +0100

    Level1

---
+++ Assets/characterCollisions.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class characterCollisions : MonoBehaviour {
+
+	private bool finished = false;
+	private bool onMenu = true;
+	[SerializeField] float time = 0;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision collision)
+	{
+		if(collision.collider.name == "end")
+		{
+			finished = true;
+			this.finishLevel();
+		}
+	}
+
+	void OnGUI()
+	{
+		if(onMenu)
+		{
+			//Show menu
+		}
+		if(finished)
+		{
+			//Show button
+		}
+	}
+
+	void finishLevel()
+	{
+		
+	}
+}
